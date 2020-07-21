6033036b5bf6f7d6e46d2c67230e8876df95f67e
Min-Ho <zocracker@icloud.com>
2020-07-21 20:08:42 +0900

    add make sns app, write models.py in sns

---
+++ sns/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from account.models import User
+
+class Staff(models.Model):
+    name     = models.CharField(max_length = 100)
+    logo     = models.ImageField(max_length = 500)
+    official = models.BooleanField()
+
+    class Meta:
+        db_table = 'staff'
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    content       = models.TextField()
+    thumbnail_img = models.URLField(max_length = 500)
+    modal_video   = models.CharField(max_length = 1500)
+    staff         = models.ForeignKey(Staff, on_delete = models.CASCADE)
+    user          = models.ManyToManyField(User, through = 'PostLike')
+
+    class Meta:
+        db_table = 'posts'
+
+    def __str__(self):
+        return self.content
+
+class PostLike(models.Model):
+    posts = models.ForeignKey(Post, on_delete = models.CASCADE)
+    users = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = 'posts_likes'
+
+class Hashtag(models.Model):
+    name  = models.CharField(max_length = 50)
+    posts = models.ManyToManyField(Post, through = 'PostHashtag')
+
+    class Meta:
+        db_table = 'hashtags'
+
+    def __str__(self):
+        return self.name
+
+class PostHashtag(models.Model):
+    posts    = models.ForeignKey(Post, on_delete = models.CASCADE)
+    hashtags = models.ForeignKey(Hashtag, on_delete = models.CASCADE)
+
+    class Meta():
+        db_table = 'posts_hashtags'
