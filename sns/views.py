32db964932a8e942e9d90c19989babad4d594db8
Min-Ho <zocracker@icloud.com>
2020-07-23 17:24:27 +0900

    ADD 29TV, sns.views.py 파일 구현 완료. DB에서 프론트에게 Json타입으로 데이터 보내기 성공

--- sns/views.py
+++ sns/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import render
+from django.http      import JsonResponse
+from django.views     import View
+
+from .models import Staff, Post, Hashtag, PostHashtag
+
+class PostView(View):
+    def get(self, request):
+        try:
+            post_list = Post.objects.select_related('staff').all()
+            data_list = []
+            for post in post_list:
+                staff_id = post.staff_id
+                staff_obj = Staff.objects.get(id=staff_id)
+                center_table_list = list(PostHashtag.objects.filter(posts_id=post.id).values())
+                hashtag_list = []
+                for obj in center_table_list:
+                    hashtag_name = Hashtag.objects.get(id=obj['hashtags_id']).name
+                    hashtag_list.append(hashtag_name)
+                post_dic = {
+                    'thumbnail_img':f'http://{post.thumbnail_img}',
+                    'staff_logo':f'http://{staff_obj.logo}',
+                    'staff_name':staff_obj.name,
+                    'official_check':staff_obj.official,
+                    'content':post.content,
+                    'hashtag':hashtag_list
+                }
+                data_list.append(post_dic)
+            return JsonResponse({'data':data_list}, status = 200)
+        except  Exception as message:
+            return JsonResponse({'message':message}, status = 401)
