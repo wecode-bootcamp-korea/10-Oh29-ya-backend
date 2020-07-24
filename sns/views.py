3dede041c61c9d6bbe99f8f0485e2dfc0338cceb
Min-Ho <zocracker@icloud.com>
2020-07-24 15:05:50 +0900

    Aquery Modeling 수정 후, views.py 파일 필드명 수정

--- sns/views.py
+++ sns/views.py
@@ -14,16 +14,16 @@ class PostView(View):
             for post in post_list:
                 staff_id = post.staff_id
                 staff_obj = Staff.objects.get(id=staff_id)
-                center_table_list = list(PostHashtag.objects.filter(posts_id=post.id).values())
+                center_table_list = list(PostHashtag.objects.filter(post_id=post.id).values())
                 hashtag_list = []
                 for obj in center_table_list:
-                    hashtag_name = Hashtag.objects.get(id=obj['hashtags_id']).name
+                    hashtag_name = Hashtag.objects.get(id=obj['hashtag_id']).name
                     hashtag_list.append(hashtag_name)
                 post_dic = {
-                    'thumbnail_img':f'http://{post.thumbnail_img}',
-                    'staff_logo':f'http://{staff_obj.logo}',
+                    'thumbnail_image':f'http://{post.thumbnail_image}',
+                    'staff_logo':f'http://{staff_obj.logo_url}',
                     'staff_name':staff_obj.name,
-                    'official_check':staff_obj.official,
+                    'official_check':staff_obj.is_official,
                     'content':post.content,
                     'hashtag':hashtag_list
                 }
