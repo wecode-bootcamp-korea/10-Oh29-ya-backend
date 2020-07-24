9d24b6a4bf5a6d3fcffaa2cd8b2bec465e59fb2e
Min-Ho <zocracker@icloud.com>
2020-07-25 02:22:01 +0900

    models.py  파일 주석 제거, views.py 파일 리팩토링 완료(리스트 컴플리헨션 적용, select_related 적용 "

--- sns/models.py
+++ sns/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-#from account.models import User
 
 class Staff(models.Model):
     name        = models.CharField(max_length = 100)
@@ -17,22 +16,12 @@ class Post(models.Model):
     content         = models.TextField()
     modal_video     = models.URLField(max_length = 2000, null = True)
     staff           = models.ForeignKey(Staff, on_delete = models.CASCADE)
-    #user            = models.ManyToManyField(User, through = 'PostLike')
 
     class Meta:
         db_table = 'posts'
 
     def __str__(self):
         return self.content
-
-'''
-class PostLike(models.Model):
-    post = models.ForeignKey(Post, on_delete = models.CASCADE)
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-
-    class Meta:
-        db_table = 'posts_likes'
-'''
 
 class Hashtag(models.Model):
     name  = models.CharField(max_length = 50)
--- sns/views.py
+++ sns/views.py
@@ -1,31 +1,30 @@
-import json
-
-from django.shortcuts import render
 from django.http      import JsonResponse
 from django.views     import View
 
-from .models import Staff, Post, Hashtag, PostHashtag
+from .models import (
+    Staff,
+    Post,
+    Hashtag,
+    PostHashtag
+)
 
 class PostView(View):
     def get(self, request):
         try:
-            post_list = Post.objects.select_related('staff').all()
+            post_list = Post.objects.select_related('staff')
             data_list = []
             for post in post_list:
-                staff_id = post.staff_id
-                staff_obj = Staff.objects.get(id=staff_id)
-                center_table_list = list(PostHashtag.objects.filter(post_id=post.id).values())
-                hashtag_list = []
-                for obj in center_table_list:
-                    hashtag_name = Hashtag.objects.get(id=obj['hashtag_id']).name
-                    hashtag_list.append(hashtag_name)
+                staff_id          = post.staff_id
+                staff_obj         = post.staff
+                center_table_list = post.posthashtag_set.values()
+                hashtag_list      = [post.hashtag_set.get(id=obj['hashtag_id']).name for obj in center_table_list]
                 post_dic = {
-                    'thumbnail_image':f'http://{post.thumbnail_image}',
-                    'staff_logo':f'http://{staff_obj.logo_url}',
-                    'staff_name':staff_obj.name,
-                    'official_check':staff_obj.is_official,
-                    'content':post.content,
-                    'hashtag':hashtag_list
+                    'thumbnail_image' : f'http://{post.thumbnail_image}',
+                    'staff_logo'      : f'http://{staff_obj.logo_url}',
+                    'staff_name'      : staff_obj.name,
+                    'official_check'  : staff_obj.is_official,
+                    'content'         : post.content,
+                    'hashtag'         : hashtag_list
                 }
                 data_list.append(post_dic)
             return JsonResponse({'data':data_list}, status = 200)
