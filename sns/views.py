9594667d0620b65f3a563e06c9423b6dea43ac8c
Min-Ho <zocracker@icloud.com>
2020-07-28 16:54:51 +0900

    views.py에서 리스트 컴프리헨션 적용

--- sns/views.py
+++ sns/views.py
@@ -12,22 +12,15 @@ class PostView(View):
     def get(self, request):
         try:
             post_list = Post.objects.select_related('staff')
-            data_list = []
-            for post in post_list:
-                staff_id          = post.staff_id
-                staff_obj         = post.staff
-                center_table_list = post.posthashtag_set.values()
-                hashtag_list      = [post.hashtag_set.get(id=obj['hashtag_id']).name for obj in center_table_list]
-                post_dic = {
-                    'id'              : post.id,
-                    'thumbnail_image' : f'http   : //{post.thumbnail_image}',
-                    'staff_logo'      : f'http://{staff_obj.logo_url}',
-                    'staff_name'      : staff_obj.name,
-                    'official_check'  : staff_obj.is_official,
-                    'content'         : post.content,
-                    'hashtag'         : hashtag_list
-                }
-                data_list.append(post_dic)
+            data_list = [{
+                'id'              : post.id,
+                'thumbnail_image' : f'http   : //{post.thumbnail_image}',
+                'staff_logo'      : f'http://{post.staff.logo_url}',
+                'staff_name'      : post.staff.name,
+                'official_check'  : post.staff.is_official,
+                'content'         : post.content,
+                'hashtag'         : [post.hashtag_set.get(id=obj['hashtag_id']).name for obj in post.posthashtag_set.values()]
+                } for post in post_list]
             return JsonResponse({'data':data_list}, status = 200)
         except  Exception as message:
             return JsonResponse({'message':message}, status = 401)
