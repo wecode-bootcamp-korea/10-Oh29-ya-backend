c4aae542497100292a8a517a1a18e48b9258435f
bangjom <kbj7227@gmail.com>
2020-07-28 17:10:32 +0900

    add feature/product

---
+++ product/urls.py
@@ -0,0 +1,14 @@
+from django.urls    import path
+from .views         import (
+    CategoryView,
+    LikeView,
+    ProductView,
+    SpecialOrderView
+)
+
+urlpatterns = [
+    path('', CategoryView.as_view()),
+    path('/like',LikeView.as_view()),
+    path('/product',ProductView.as_view()),
+    path('/specialorders',SpecialOrderView.as_view())
+]
