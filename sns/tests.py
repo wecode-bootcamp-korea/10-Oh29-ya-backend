655b8a2caebf8a47dd59c33e4d0a881b1afc9359
Min-Ho <zocracker@icloud.com>
2020-07-28 16:07:51 +0900

    리팩토링 및 test.py 일부 추가

--- sns/tests.py
+++ sns/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+clinet = Client()
+
+class TestPostView(TestCase):
+    def test_post_postview(self):
+        response = clinet.get('/sns')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'data':data_list})
