4213f6b8be20fee67ea622a0c99dfc516f7f1cf0
Min-Ho <zocracker@icloud.com>
2020-07-29 11:54:33 +0900

    아름다운 위코드 스타일로 컨벤션

--- product/views.py
+++ product/views.py
@@ -1,26 +1,32 @@
-import bcrypt
-import hashlib
-import json
-import jwt
-import os
-import requests
-import sys
+import (
+    bcrypt,
+    hashlib,
+    json,
+    jwt,
+    os,
+    requests,
+    sys
+)
 
-from django.http                import JsonResponse
-from django.core.validators     import (
+from django.http  import JsonResponse
+from django.views import View
+
+from django.core.validators import (
     validate_email,
     RegexValidator
 )
-from django.core.exceptions     import (
+
+from django.core.exceptions import (
     ValidationError,
     ObjectDoesNotExist
 )
-from django.views               import View
-from my_settings                import (
+
+from my_settings import (
     SECRET_KEY,
     ALGORITHM
 )
-from .models                    import (
+
+from .models import (
     Brand,
     Category,
     Subcategory,
@@ -30,13 +36,13 @@ from .models                    import (
     LikeProduct,
     SpecialOrder
 )
-from account.models             import User
-from account.utils              import (
+
+from account.models import User
+
+from account.utils import (
     login_decorator,
     detoken
 )
-
-
 
 class CategoryView(View):
     def get(self, request):
@@ -47,37 +53,35 @@ class CategoryView(View):
             data["subcategory"] = request.GET.get("subcategory",None)
             product             = Product.objects
             products            = product.filter(category=Category.objects.get(name=data["category"]),subcategory=Subcategory.objects.get(name=data["subcategory"]))
-            productList=[
-                {
-                    'id'                : word.id,
-                    'name'              : word.name,
-                    'price'             : word.price,
-                    'discount_rate'     : word.discount_rate,
-                    'discount_price'    : word.discount_price,
-                    'brand'             : word.brand.name,
-                    'image'             : [image.image for image in Image.objects.filter(product_id = word.id)],
-                    'like_num'          : word.like_num,
-                    'user_like_pressed' : False
-                } for word in products   
-            ]
+            productList=[{
+                'id'                : word.id,
+                'name'              : word.name,
+                'price'             : word.price,
+                'discount_rate'     : word.discount_rate,
+                'discount_price'    : word.discount_price,
+                'brand'             : word.brand.name,
+                'image'             : [image.image for image in Image.objects.filter(product_id = word.id)],
+                'like_num'          : word.like_num,
+                'user_like_pressed' : False
+            } for word in products]
             if user:
-                for  temp in productList:
+                for temp in productList:
                     temp['user_like_pressed'] = (True if LikeProduct.objects.filter(user = User.objects.get(id = user.id),product = Product.objects.get(id = temp['id'])).exists() else False)   
             return JsonResponse({"data":productList} , status = 200)
         except TypeError:
-            return JsonResponse({'message':"TYPEERROR"}, status = 400)
+            return JsonResponse({'message':"TYPE_ERROR"}, status = 400)
         except ObjectDoesNotExist:
             return JsonResponse({'message':"DOES_NOT_EXIST"}, status = 400)
 
 class LikeView(View):
     @login_decorator
-    def patch(self, request):  
+    def patch(self, request):
         data    = json.loads(request.body)
         user    = request.user
         product = Product.objects.get(id = data["product"])
         try:
-            if LikeProduct.objects.filter(user = user.id, product = data['product']).exists():
-               LikeProduct.objects.get(user = user.id, product = data['product']).delete()
+            if LikeProduct.objects.filter(user=user.id, product=data['product']).exists():
+               LikeProduct.objects.get(user=user.id, product=data['product']).delete()
                product.like_num-=1
                product.save()
             else:
@@ -93,10 +97,10 @@ class LikeView(View):
         except json.decoder.JSONDecodeError:
             return JsonResponse({'message':"JSON_ERROR"}, status = 400)
         except ValueError:
-            return JsonResponse({'message':"VALUEERROR"}, status = 400)
+            return JsonResponse({'message':"VALUE_ERROR"}, status = 400)
 
 class ProductView(View):
-    def get(self, request, product_id): 
+    def get(self, request, product_id):
         user = detoken(request)
         try:
             product = Product.objects.prefetch_related('image_set').select_related('brand', 'category', 'subcategory', 'detail').get(id=product_id)
@@ -131,27 +135,25 @@ class SpecialOrderView(View):
     def get(self, request):
         try:
             special_orders = SpecialOrder.objects.select_related('product__brand')
-            productList=[
-                {
-                    'id'            : word.id,
-                    'title'         : word.title,
-                    'subtitle'      : word.subtitle,
-                    'image'         : word.image,
-                    'start'         : word.time.split('~')[0].replace('.','/').rstrip(),
-                    'end'           : word.time.split('~')[1].replace('.','/'),
-                    'name'          : word.product.name,
-                    'price'         : word.product.price,
-                    'product_id'    : word.product.id,
-                    'discount_rate' : word.product.discount_rate,
-                    'discount_price': word.product.discount_price,
-                    'brand'         : word.product.brand.name,
-                    'like_num'      : word.product.like_num 
-                } for word in special_orders   
-            ]    
+            productList=[{
+                'id'             : word.id,
+                'title'          : word.title,
+                'subtitle'       : word.subtitle,
+                'image'          : word.image,
+                'start'          : word.time.split('~')[0].replace('.','/').rstrip(),
+                'end'            : word.time.split('~')[1].replace('.','/'),
+                'name'           : word.product.name,
+                'price'          : word.product.price,
+                'product_id'     : word.product.id,
+                'discount_rate'  : word.product.discount_rate,
+                'discount_price' : word.product.discount_price,
+                'brand'          : word.product.brand.name,
+                'like_num'       : word.product.like_num
+            } for word in special_orders]
             return JsonResponse({"data":productList} , status = 200)
         except ObjectDoesNotExist:
             return JsonResponse({'message':"DOES_NOT_EXIST"}, status = 400)
         except json.decoder.JSONDecodeError:
             return JsonResponse({'message':"JSON_ERROR"}, status = 400)
         except ValueError:
-            return JsonResponse({'message':"VALUEERROR"}, status = 400)
+            return JsonResponse({'message':"VALUE_ERROR"}, status = 400)
