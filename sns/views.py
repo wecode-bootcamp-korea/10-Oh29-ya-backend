e57396122df58619bc6d3450c5ce2e01b0702ad7
Min-Ho <zocracker@icloud.com>
2020-07-29 17:54:17 +0900

    sns/views.py에서 PostView는 로그인을 안해도 볼 수 있도록, 대신 하느틑 당연히 색이 없고 타인이 좋아요를 누른 하트의 개수는 남아있음.

--- sns/views.py
+++ sns/views.py
@@ -13,14 +13,17 @@ from .models import (
 )
 
 from account.models import User
+from account.utils import (
+    login_decorator,
+    detoken
+)
 
 class PostView(View):
-    def post(self, request):
+    def get(self, request):
         try:
-            data = json.loads(request.body)
+            user      = detoken(request)
             post_list = Post.objects.select_related('staff')
             data_list = [{
-                'user_id'            : data['user'],
                 'post_id'            : post.id,
                 'thumbnail_image'    : f'http://{post.thumbnail_image}',
                 'staff_logo'         : f'http://{post.staff.logo_url}',
@@ -29,25 +32,30 @@ class PostView(View):
                 'content'            : post.content,
                 'hashtag'            : [post.hashtag_set.get(id=obj['hashtag_id']).name for obj in post.posthashtag_set.values()],
                 'like_num'           : post.like_num,
-                'user_likes_pressed' : (True if LikePost.objects.filter(user_id=User.objects.get(id=data['user']).id, post_id=Post.objects.get(id=post.id).id).exists() else False)
+                'user_likes_pressed' : False
             } for post in post_list]
+            if user:
+                for tmp in data_list:
+                    tmp['user_likes_pressed'] = (True if LikePost.objects.filter(user_id=User.objects.get(id=user.id).id, post_id=Post.objects.get(id=tmp['post_id']).id).exists() else False)
             return JsonResponse({'data':data_list}, status = 200)
         except  Exception as message:
             return JsonResponse({'message':message}, status = 401)
 
 class LikePostView(View):
+    @login_decorator
     def patch(self, request):
+        data = json.loads(request.body)
+        user = request.user
+        post = Post.objects.get(id=data['post'])
         try:
-            data = json.loads(request.body)
-            post = Post.objects.get(id=data['post'])
-            if LikePost.objects.filter(user_id=data['user'], post_id=data['post']).exists():
-                LikePost.objects.get(user_id=data['user'], post_id=data['post']).delete()
+            if LikePost.objects.filter(user_id=user.id, post_id=data['post']).exists():
+                LikePost.objects.get(user_id=user.id, post_id=data['post']).delete()
                 post.like_num = len(list(LikePost.objects.filter(post_id=Post.objects.get(id=post.id))))
                 post.save()
             else:
                 LikePost(
                     post_id = post.id,
-                    user_id = User.objects.get(id=data['user']).id
+                    user_id = User.objects.get(id=user.id).id
                 ).save()
                 post.like_num = len(list(LikePost.objects.filter(post_id=Post.objects.get(id=post.id))))
                 post.save()
