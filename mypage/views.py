700e40789e167c9534a950b7ac4dc3477082882b
Min-Ho <zocracker@icloud.com>
2020-07-29 18:36:08 +0900

    mypage/views.py에 원래 좋아요한 상품들만 보이는 뷰 였는데, 거기에 포스트(29티비)를 좋아한 것도 나오게 구현

--- mypage/views.py
+++ mypage/views.py
@@ -37,27 +37,32 @@ from account.utils  import login_decorator
 class HeartProductView(View):
     @login_decorator
     def get(self, request):
+        try:
             user        = request.user
-            products    = LikeProduct.objects.select_related('user').select_related('product').filter(user_id=user)
+            products    = LikeProduct.objects.select_related('user', 'product').filter(user_id=user.id)
             productList=[{
-                'id'            : word.product.id,
-                'name'          : word.product.name,
-                'price'         : word.product.price,
-                'discount_rate' : word.product.discount_rate,
-                'discount_price': word.product.discount_price,
-                'brand'         : word.product.brand.name,
-                'image'         : [image.image for image in Image.objects.filter(product_id=word.product.id)],
-                'like_num'      : word.product.like_num
+                'id'                 : word.product.id,
+                'name'               : word.product.name,
+                'price'              : word.product.price,
+                'discount_rate'      : word.product.discount_rate,
+                'discount_price'     : word.product.discount_price,
+                'brand'              : word.product.brand.name,
+                'image'              : [image.image for image in Image.objects.filter(product_id=word.product.id)],
+                'like_num'           : word.product.like_num,
+                'user_likes_pressed' : (True if LikeProduct.objects.filter(user_id=User.objects.get(id=user.id).id, product_id=Product.objects.get(id=word.product.id).id).exists() else False)
             } for word in products]
             return JsonResponse({"data":productList} ,status=200)
+        except Exception as e:
+            return JsonResponse({'message':e}, status = 200)
 
 class HeartPostView(View):
-    def post(self, request):
+    @login_decorator
+    def get(self, request):
         try:
-            data = json.loads(request.body)
-            user_likes = User.objects.prefetch_related('likepost_set').get(id=data['user']).likepost_set.all()
+            user       = request.user
+            user_likes = User.objects.prefetch_related('likepost_set').get(id=user.id).likepost_set.all()
             my_heart_list = [{
-                'user_id'            : data['user'],
+                'user_id'            : user.id,
                 'post_id'            : post.post.id,
                 'thumbnail_image'    : f'http://{post.post.thumbnail_image}',
                 'staff_logo'         : f'http://{post.post.staff.logo_url}',
@@ -66,7 +71,7 @@ class HeartPostView(View):
                 'content'            : post.post.content,
                 'hashtag'            : [inter_obj.hashtag.name for inter_obj in post.post.posthashtag_set.all()],
                 'like_num'           : post.post.like_num,
-                'user_likes_pressed' : (True if LikePost.objects.filter(user_id=User.objects.get(id=data['user']).id,
+                'user_likes_pressed' : (True if LikePost.objects.filter(user_id=User.objects.get(id=user.id).id,
 post_id=Post.objects.get(id=post.post.id).id).exists() else False)
             } for post in user_likes]
             return JsonResponse({'my_heart_list':my_heart_list}, status = 200)
